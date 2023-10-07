cb1758b58d2350e06ecf39c72640e2aeabc6584e
northalligator <eekret@yandex.ru>
2023-10-07 19:32:06 +0300

    +start

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from config.py import bot_token
 
 bot = telebot.TeleBot(bot_token)
 #test
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,"test")
